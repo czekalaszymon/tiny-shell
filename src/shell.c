9dc2562a1047b8baea7a4d72a4bdb7b999aad7ee
Szymon Czekala <czekalaszymon.contact@gmail.com>
2025-07-11 15:30:39 +0200

    Refactor shell into separate files
    Split main logic into shell.c and shell.h before the code
    grows too complex to maintain in one file.

--- src/shell.c
+++ src/shell.c
@@ -1,9 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
 #include "shell.h"
 
-void parse_input(char* input, char** argv) {
-	// TODO
+void parse_input(char* input, char** argv)
+{
+	char* token;
+	int i = 0;
+
+	token = strtok(input, " ");
+	while (token != NULL && i < 15) {
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
 }
 
-void run_command(char** argv) {
-	// TODO
+void run_command(char** argv)
+{
+	pid_t pid = fork();
+	if (pid == 0) {
+		// Child process
+		execvp(argv[0], argv);
+		perror("exec failed");
+		exit(1);
+	} else if (pid > 0)
+		// Parent process
+		waitpid(pid, NULL, 0);
+	else
+		perror("fork failed");
 }
